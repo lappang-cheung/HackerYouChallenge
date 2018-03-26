a62543dda1e5c2d7b1406ead80e0d927077ad636
Leo Cheung <leocheung_1986@hotmail.com>
2018-03-25 21:32:29 -0400

    breaking list with into another template

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import ListItem from './ListItem';
 
 class List extends Component
 {
+
+    state = {
+        beers: [],
+        store: []
+    }
+
+    componentWillMount()
+    {
+        axios.get('/list').then((res) => {
+            this.setState({ beers: res.data.result });
+
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
     render()
     {
         return(
             <div>
                 <h1>This is the Beer List page</h1>
+                <ListItem />
             </div>
         )
     }
---
+++ client/src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ListItem = () => {
+    return (
+        <div>
+            <p>List Item</p>
+        </div>
+    )
+}
+
+export default ListItem;
